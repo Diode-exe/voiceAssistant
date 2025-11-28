a3ec5c82360e7f97ec9a09640a089190375fb838
Diode-exe <DiodesCAER@protonmail.com>
2025-11-28 15:25:46 -0600

    it speaks :O

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import webbrowser
 from battery import Batteries
 import pyautogui
 import time
-
-# Source - https://stackoverflow.com/a/10441322
-# Posted by Skurmedel, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-11-26, License - CC BY-SA 4.0
+import pyttsx3
+import threading
+import re
+import queue
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
@@ -23,9 +23,24 @@ c = wmi.WMI()
 t = wmi.WMI(moniker = "//./root/wmi")
 
 r = sr.Recognizer()
+engine = pyttsx3.init()
+speech_queue = queue.Queue()
 
 stop = False
 
+def speak(text):
+    speech_queue.put(text)
+
+def tts_worker():
+    while True:
+        text = speech_queue.get()
+        if text is None:
+            break
+        engine.say(text)
+        engine.runAndWait()
+        speech_queue.task_done()
+
+threading.Thread(target=tts_worker, daemon=True).start()
 
 def playpause():
     hwcode = win32api.MapVirtualKey(win32con.VK_MEDIA_PLAY_PAUSE, 0)
@@ -43,21 +58,39 @@ def decreaseVol(step=0.06):
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     vol = cast(interface, POINTER(IAudioEndpointVolume))
-
     current = vol.GetMasterVolumeLevelScalar()
     new = max(current - step, 0.0)
-
     vol.SetMasterVolumeLevelScalar(new, None)
 
 def increaseVol(step=0.06):
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     vol = cast(interface, POINTER(IAudioEndpointVolume))
-
     current = vol.GetMasterVolumeLevelScalar()
     new = max(current + step, 0.0)
-
     vol.SetMasterVolumeLevelScalar(new, None)
+
+def parseSwitchWindows():
+    numbers = {
+        "one": 1, "two": 2, "three": 3, "four": 4,
+        "five": 5, "six": 6, "seven": 7, "eight": 8,
+        "nine": 9, "ten": 10
+    }
+
+    match = re.search(r'\d+', text)
+    if match:
+        times = int(match.group())
+    else:
+        # check for spelled out number
+        for word, num in numbers.items():
+            if word in text:
+                times = num
+                break
+        else:
+            times = 1
+
+    for _ in range(times):
+        switchWindows()
 
 def switchWindows():
     pyautogui.keyDown('alt')
@@ -73,35 +106,50 @@ while not stop:
         text = r.recognize_google(audio).lower()
 
         print(f"I heard {text}")
+        speak(f"I heard {text}")
 
         if "pause" in text or "play" in text:
-            print("Ok, pausing")
+            msg = "Ok, pausing"
+            print(msg)
+            speak(msg)
             playpause()
         elif "next" in text:
             next_track()
         elif "previous" in text:
             prev_track()
         elif "open" in text and "browser" in text:
-            print("Ok, opening your browser")
+            msg = "Ok, opening your browser"
+            print(msg)
+            speak(msg)
             webbrowser.open_new_tab("https://blank.org")
         elif "time" in text:
-            print(f"Ok, the time is {datetime.datetime.now()}")
+            msg = f"Ok, the time is {datetime.datetime.now()}"
+            print(msg)
+            speak(msg)
         elif "stop" in text:
-            print("Ok, exiting")
+            msg = "Ok, exiting"
+            print(msg)
+            speak(msg)
             stop = True
         elif "volume down" in text:
             decreaseVol()
         elif "volume up" in text:
             increaseVol()
         elif "test" in text:
-            print("Test received. It's working.")
+            msg = "Test received. It's working."
+            print(msg)
+            speak(msg)
         elif "battery" in text:
             Batteries.getBattery()
         elif "start menu" in text:
             pyautogui.press('win')
         elif "switch windows" in text or "switch window" in text:
-            switchWindows()
+            parseSwitchWindows()
         else:
-            print("I don't understand...")
+            msg = "I don't understand..."
+            print(msg)
+            speak(msg)
     except sr.exceptions.UnknownValueError:
-        print("Sorry, I didn't catch that")
+        msg = "Sorry, I didn't catch that"
+        print(msg)
+        speak(msg)
